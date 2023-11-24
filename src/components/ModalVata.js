59a3eeeb208e3e71e7d009849e03e7857e5b98be
uguumur <uguumur.oldokh@gmail.com>
2023-11-24 09:01:35 -0600

    modal

--- src/components/ModalVata.js
+++ src/components/ModalVata.js
@@ -7,7 +7,7 @@ const Modal = ({ open, onClose, children, ...props }) => {
     <AnimatePresence>
       {open && (
         <motion.div
-          className="bg-black z-[9999] p-4 w-full absolute h-full bottom-0 left-0 flex items-center"
+          className="bg-black z-[9999] p-4 w-full absolute h-screen bottom-0 left-0 flex items-center"
           initial={{ y: "100%" }}
           animate={{ y: 0 }}
           exit={{ y: "100%" }}
